fcf2cf2c1e385df67e637c61510b203e8424cc6e
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-07-01 16:08:14 -0400

    feat: add card component and testing data

---
+++ src/components/Card.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let country;
+  export let flagImage;
+  export let flagAlt;
+  export let population;
+  export let region;
+  export let capital;
+</script>
+
+<div>
+  <img src={flagImage} alt={flagAlt} />
+  <h2>{country}</h2>
+  <p>Population: <span>{population}</span></p>
+  <p>Region: <span>{region}</span></p>
+  <p>Capital: <span>{capital}</span></p>
+</div>
