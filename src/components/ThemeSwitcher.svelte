313a4eb97b53fdf00cd0f5f9128408ac24ddbb57
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-07-01 12:07:18 -0400

    feat: create theme switcher

--- src/components/ThemeSwitcher.svelte
+++ src/components/ThemeSwitcher.svelte
@@ -1,3 +1,20 @@
-<script></script>
+<script>
+  let theme = localStorage.getItem('theme') ?? 'light';
 
-<div class="bg-slate-800">theme switcher</div>
+  function handleClick() {
+    theme === 'light' ? (theme = 'dark') : (theme = 'light');
+  }
+
+  $: {
+    theme === 'dark'
+      ? document.documentElement.classList.add('dark')
+      : document.documentElement.classList.remove('dark');
+    localStorage.setItem('theme', theme);
+  }
+</script>
+
+<div>
+  <button on:click={handleClick}>
+    {theme === 'light' ? '🌙' : '🌞'}
+  </button>
+</div>
