53e18358e8084abfa044dffbdfdfd787dbcb3abc
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-07-01 11:15:16 -0400

    feat: set up challenge

---
+++ src/components/ThemeSwitcher.svelte
@@ -0,0 +1,3 @@
+<script></script>
+
+<div class="bg-slate-800">theme switcher</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'elements-dark-blue': 'hsl(209, 23%, 22%)',
+        'bg-very-dark-blue': 'hsl(207, 26%, 17%)',
+        'text-very-dark-blue': 'hsl(200, 15%, 8%)',
+        'input-dark-gray': 'hsl(0, 0%, 52%)',
+        'bg-very-light-gray': 'hsl(0, 0%, 98%)',
+        white: 'hsl(0, 0%, 98%)',
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [],
+};
