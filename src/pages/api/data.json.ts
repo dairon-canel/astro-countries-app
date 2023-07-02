f3dfc40e483e40f9be6a70489e10d2257f03ae75
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-07-02 13:45:25 -0400

    feat: add ssg

--- src/pages/api/data.json.ts
+++ src/pages/api/data.json.ts
@@ -6,8 +6,15 @@ export interface CardData {
   flag: string;
   population: number;
   region: string;
+  nativeName: string;
+  subRegion: string;
+  topLevelDomain: string[];
+  languages: string[];
+  currencies?: string[];
+  borderCountries?: string[];
   capital?: string;
 }
+
 /* <Card
         className="mx-14 mb-10"
         client:visible
@@ -19,13 +26,21 @@ export interface CardData {
         capital={e.capital}
       /> */
 
-export const get: APIRoute = async function get({ params, request }) {
+export const get: APIRoute = async function get({ params, request, props }) {
   const allCardData: CardData[] = data.map(country => {
     return {
       name: country.name,
       flag: country.flags.svg,
       population: country.population,
       region: country.region,
+      nativeName: country.nativeName,
+      subRegion: country.subregion,
+      topLevelDomain: country.topLevelDomain,
+      currencies: country.currencies?.map(currency => currency.name),
+      languages: country.languages?.map(language => language.name),
+      borderCountries: country.borders?.map(
+        border => data.find(e => e.alpha3Code === border)?.name,
+      ),
       capital: country.capital,
     };
   });
